a754fd0131301a531528bdd1424c4638b5c3c7e3
Venkat Ram Taddi <tadivenkat@gmail.com>
2025-01-17 11:04:42 -0600

    Using environment variables for firebase config properties

--- mvp-starter/firebase/firebase.js
+++ mvp-starter/firebase/firebase.js
@@ -27,14 +27,16 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCGSyFDu3veO-lNr0rS9MOgAOmlt6Dk59g",
-  authDomain: "expenses-tracker-dev-2648c.firebaseapp.com",
-  projectId: "expenses-tracker-dev-2648c",
-  storageBucket: "expenses-tracker-dev-2648c.firebasestorage.app",
-  messagingSenderId: "142641161416",
-  appId: "1:142641161416:web:929df1ac194578d94d71fd",
-  measurementId: "G-C3WX2K03L6"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
+
+console.log(firebaseConfig);
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
